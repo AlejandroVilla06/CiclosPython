4034616874715e34d0787da8d276808c240272da
Henry <john_henry.4m@hotmail.com>
2023-08-25 11:46:46 -0500

    NT3 Ciclos clase 3

--- peluchitos.py
+++ peluchitos.py
@@ -10,20 +10,21 @@ print("4. Ver productos mas vendidos")
 print("5. SALIR")
 
 while(opcion!=5):
-    print=int(input("Digita un numero: ")) 
+    opcion=int(input("Digita un numero: ")) 
     
     if(opcion==1):
-        print("Usted esta en la opcion 1")
+        nombre=input("Digita el nombre del producto: ")
+        #agregando una lista o arreglo
+        peluches.append(nombre)
+        print("Peluche agregado correctamente.....")
     elif(opcion==2):
-        print("Usted esta en la opcion 2")    
+        print(peluches)
     elif(opcion==3):
         print("Usted esta en la opcion 3")
     elif(opcion==4):
         print("Usted esta en la opcion 4")
-    elif(opcion==5):
-        print("Hasta Luego")
     else:
-       print("La opcion que elegiste no existe")
+        print("La opcion no existe")
     
                   
 print("Sali del ciclo")
