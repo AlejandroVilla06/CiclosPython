24693d295878ddd830350fd6c6802889bf860748
Henry <john_henry.4m@hotmail.com>
2023-08-25 11:17:41 -0500

    NT4 se crean ciclos

---
+++ peluchitos.py
@@ -0,0 +1,30 @@
+peluches=["roshi","conejito","aguacatico"]
+
+opcion=0
+print("Pelucheria PELUCHITOS")
+print("..............")
+print("1. Agregar producto a la bodega")
+print("2. Ver productos en bodega")
+print("3. Obtener valor del inventario")
+print("4. Ver productos mas vendidos")
+print("5. SALIR")
+
+while(opcion!=5):
+    print=int(input("Digita un numero: ")) 
+    
+    if(opcion==1):
+        print("Usted esta en la opcion 1")
+    elif(opcion==2):
+        print("Usted esta en la opcion 2")    
+    elif(opcion==3):
+        print("Usted esta en la opcion 3")
+    elif(opcion==4):
+        print("Usted esta en la opcion 4")
+    elif(opcion==5):
+        print("Hasta Luego")
+    else:
+       print("La opcion que elegiste no existe")
+    
+                  
+print("Sali del ciclo")
+    
